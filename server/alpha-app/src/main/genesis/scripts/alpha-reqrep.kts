3eb4f3b5f6c1b830a65c78a066c6a16296c9a42c
Guilherme Guerra Martins <guilherme.guerra@genesis.global>
2024-05-03 17:03:29 +0000

    web handler

--- server/alpha-app/src/main/genesis/scripts/alpha-reqrep.kts
+++ server/alpha-app/src/main/genesis/scripts/alpha-reqrep.kts
@@ -1,3 +1,36 @@
+
+data class InputClass(val field1: String, val field2: Int?)
+data class OutputClass(val message: String, val details: String)
+
 requestReplies{
+
+    requestReply<InputClass, OutputClass>("CUSTOM_REQUEST"){
+        replySingle{ input ->
+            OutputClass(input.field1, "This is a number: ${input.field2}")
+        }
+    }
+
+
+    requestReply("TRAINING_REQUEST",TRAINING_VIEW)
+
+    requestReply("TRADE_DETAILS",TRADE){
+        request{
+            COUNTERPARTY_ID withTransformation { type, _ ->
+                if (type == "2"){
+                    "UNKOWN"
+                }else {
+                    type
+                }
+            }
+        }
+        reply{
+            TRADE_ID
+            COUNTERPARTY_ID
+            INSTRUMENT_ID
+            QUANTITY
+            PRICE
+        }
+    }
     requestReply("TRAINING_REQUEST",TRAINING_VIEW)
 }
+
---
+++ server/alpha-app/src/main/genesis/scripts/alpha-web-handler.kts
@@ -0,0 +1,43 @@
+import global.genesis.commons.standards.GenesisPaths
+import global.genesis.message.core.HttpStatusCode
+import java.nio.file.Paths
+import java.io.File
+
+
+webHandlers("BASE-PATH"){
+    multipartEndpoint("FILE_UPLOAD"){
+        val fileUploadFolder = "training/files"
+        config {
+            multiPart {
+                maxFileSize = 10_000_000
+                useDisk = true
+                baseDir = fileUploadFolder
+            }
+        }
+
+        val savedFolder = Paths.get(GenesisPaths.runtime() + "/" + fileUploadFolder)
+
+        handleRequest {
+            body.fileUploads.forEach {
+                val file = File("$savedFolder/${it.fileName}")
+                require(!file.exists()) { "File already exists" }
+                it.copyTo(savedFolder.resolve(it.fileName))
+            }
+        }
+        exceptionHandler<IllegalArgumentException>(HttpStatusCode.Forbidden){
+            exception.message ?: "Error"
+        }
+    }
+
+    endpoint(GET, "ALL-TRADES"){
+        val searchTradeId by header("SEARCH_TRADE_ID")
+        handleRequest {
+            require(searchTradeId != "1") { "searchTradeId cannot be 1"}
+            db.get(Trade.byId(searchTradeId))
+        }
+        exceptionHandler<IllegalArgumentException>{
+            exception.message ?: "Error performing the search"
+        }
+    }
+
+}
